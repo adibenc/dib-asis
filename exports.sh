d3b04ddda33a69c64c8c2c3aa556ef59607341cc
adib-enc <adib35785@gmail.com>
2022-09-07 13:44:25 +0700

    export usr bin bin

--- exports.sh
+++ exports.sh
@@ -1,7 +1,7 @@
 #exports
 #flutter & go
 export PATH="$PATH:/media/data3/flutter/bin\
-:/usr/local/go/bin:/media/data3/projects/tunnelroot\
+:/usr/local/go/bin:/usr/bin/bin/:/media/data3/projects/tunnelroot\
 :/media/data1/installs/4linux/jre-8u311-linux-x64/jre1.8.0_311/bin\
 :/media/data1/installs/4linux/node-v14.15.4-linux-x64/bin:/opt/lampp/bin/"
 # :/media/data3/Installsrc/4linux/node-v14.15.4-linux-x64/bin
