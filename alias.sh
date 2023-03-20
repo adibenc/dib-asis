42a41798f473b9b5e00c8037b10835cebe4eb28b
adibwenc <adib35785@gmail.com>
2023-03-20 10:02:57 +0700

    jwt decoder

--- alias.sh
+++ alias.sh
@@ -194,6 +194,14 @@ function initMconda(){
 	# <<< conda initialize <<<
 }
 
+# https://prefetch.net/blog/2020/07/14/decoding-json-web-tokens-jwts-from-the-linux-command-line/
+function jwtd() {
+    if [[ -x $(command -v jq) ]]; then
+         jq -R 'split(".") | .[0],.[1] | @base64d | fromjson' <<< "${1}"
+         echo "Signature: $(echo "${1}" | awk -F'.' '{print $3}')"
+    fi
+}
+
 #initConda
 
 __secret=$__dir"/secrets"
