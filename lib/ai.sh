02a2599c82242549f05650b50fc695395f753832
adib-enc <adib35785@gmail.com>
2025-02-09 19:19:58 +0700

    add deep seek

--- lib/ai.sh
+++ lib/ai.sh
@@ -10,3 +10,29 @@ llm-gm1(){
 	llm -m gemini-1.5-flash-latest --key $GEMINI_API_KEY $quest
 }
 
+llm-ds(){
+	quest=$1
+	llm -m deepseek --key $DEEPSEEK_API_KEY $quest
+}
+
+: "
+llm -m deepseek-chat 'five great names for a pet ocelot'
+llm -m deepseek-reasoner 'solve \\int \\frac{\\ln(x)\\arctan(x)}{x^2+1} dx'
+llm -m deepseek-coder 'how to reverse a linked list in python'
+"
+# llm -m gemini-1.5-flash-latest 'explain ttx'
+llm-dsc(){
+	quest=$1
+	llm -m deepseek-chat --key $DEEPSEEK_API_KEY $quest
+}
+
+llm-dsr(){
+	quest=$1
+	llm -m deepseek-reasoner --key $DEEPSEEK_API_KEY $quest
+}
+
+llm-dscoder(){
+	quest=$1
+	llm -m deepseek-coder --key $DEEPSEEK_API_KEY $quest
+}
+
